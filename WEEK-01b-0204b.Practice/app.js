71f0f52031f31b5e4374b519878d643b8d093bf8
Santiago v <santiagovanegasoa@gmail.com>
2024-09-05 14:33:28 +0200

    exercise  04b practice

---
+++ WEEK-01b-0204b.Practice/app.js
@@ -0,0 +1,27 @@
+var a = "3";
+var b = "8";
+
+/***********Do not change the code above 👆*******/
+//Write your code here :
+
+let  swtich = a ;
+a = b ; 
+b = swtich;
+
+
+/***********Do not change the code below 👇*******/
+
+console.log("a is " + a);
+console.log("b is " + b);
+
+/*The variable a shoudl hold the value "8".
+
+And the variable b should hold the value "3".
+
+When the code is run, it should output:
+
+a is 8
+
+b is 3
+
+Copy the bellow code into your javascript file and tun it in the browser.*/
